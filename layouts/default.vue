3283019d48ad1df0469f7d03158a3d61584110a7
Simerca <contactsimerca@gmail.com>
2022-07-17 01:33:29 +0200

    First commit 🚀

---
+++ layouts/default.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-app-bar fixed style="background-color:rgba(0,0,0,0);" elevation="0" height="100" app>
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <img height="50" src="/img/logo-color.png" />
+          </v-col>
+        </v-row>
+      </v-app-bar>
+      <v-container fluid class="px-0">
+        <v-carousel :show-arrows="false">
+          <v-carousel-item src="/img/screenshot.png">
+            <v-container>
+            <v-row>
+              <v-col cols="12" md="6" class="py-16 mt-12">
+                <v-card class="rounded-lg pa-6">
+                  <v-card-text>
+                    <span class="text-h4">Claim your <span class="primary--text">LAND</span> for Free now</span>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn color="accent" class="font-weight-bold" width="200">Claim</v-btn>
+                    <v-spacer />
+                    <v-btn color="secondary" class="font-weight-bold" width="200">Play</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-col> 
+            </v-row>
+            </v-container>
+          </v-carousel-item>
+        </v-carousel>
+      </v-container>
+        <Nuxt />
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+    <v-container class="py-12">
+      <v-row>
+        <v-col cols=3>
+          <img width="200px" src="/img/logo-white.png"/>
+        </v-col>
+        <v-col>
+          Home <br>
+        </v-col>
+        <v-col class="text-right">
+          <span class="font-weight-bold text-h5">
+            Follow us
+          </span>
+          <br/>
+          <v-btn icon>
+            <v-icon>mdi-discord</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-twitter</v-icon>
+            </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
+$body-font-family: 'Open Sans';
+$title-font: 'Alfa Slab One';
+
+.v-application {
+   font-family: $body-font-family, sans-serif !important;
+    .title, .text-h1, .text-h2,.text-h3,.text-h4,.text-h5 { // To pin point specific classes of some components
+       font-family: $title-font, sans-serif !important;
+    }
+ }
+
+.theme--dark.v-application{
+  background: rgb(2,0,36);
+  background: linear-gradient(150deg, rgba(2,0,36,1) 0%, rgba(3,28,8,1) 100%);
+}
+ 
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<template>
+<v-container fluid class="px-0">
+  <v-container>
+    <v-row>
+      <v-col cols="8">
+        <span class="text-h3  primary--text">Community</span> <br/>
+        <span  class="text-h3 ">What's new ?</span>
+      </v-col>
+      <v-col class="py-12 text-right">
+        Read more ...
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="4" v-for="i in 3" :key="i"> 
+        <v-card class="rounded">
+          <v-img src="/img/screenshot.png" />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-sheet class="mt-16 py-16" style="position:relative; background-image:url('/img/screenshot.png');" height=600>
+   <v-row class="mt-16">
+    <v-col cols="10" md="8" class="mx-auto py-12">
+      <v-card class="rounded-lg">
+        <v-card-text class="pa-12">
+          <span class="text-h3">
+            For limited time we offer to you a <span class="primary--text">LAND</span>
+          </span>
+          <v-text-field rounded class="rounded-sm mt-8" dense filled placeholder="Enter your email" />
+          <v-btn color="accent" block>Claim</v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+   </v-row>
+  </v-sheet>
+
+<v-container class="mt-12">
+  <v-row>
+  <v-col cols=8 class="text-center mx-auto">
+    <span class="text-h3 ">Join our <span class="primary--text">Community</span></span>
+  </v-col>
+  <v-col cols="8" class="py-16 mx-auto">
+    <v-btn block><v-icon class="mr-3">mdi-discord</v-icon>Join our discord</v-btn>
+  </v-col>
+  </v-row>
+</v-container>  
+
+<v-sheet height="400" class="mt-12 py-16" style="background-image:url('/img/screenshot.png')">
+  <v-row>
+    <v-col class="text-center" cols="12">
+      <span class="text-h3">Join our <span class="primary--text">World</span></span>
+    </v-col>
+    <v-col cols="6" class="text-center mt-12">
+      <v-btn><v-icon class="mr-3">mdi-apple</v-icon>Download for Mac</v-btn>
+    </v-col>
+    <v-col cols="6" class="text-center mt-12">
+      <v-btn><v-icon class="mr-3">mdi-microsoft</v-icon>Download for Windows</v-btn>
+    </v-col>
+  </v-row>
+</v-sheet>
+
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
