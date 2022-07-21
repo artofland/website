e81628df6c87380a9180f4743491776d88690d3e
Simerca <contactsimerca@gmail.com>
2022-07-21 03:28:37 +0200

    Add og tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,18 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Let your creativity take over, build without limit in your metaverse. Play with friends and the whole world' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+      { hid: 'og-title', property: 'og:title', content: 'Artof.Land Buy and Sell Metaverse Land' },
+      { hid: 'og-desc', property: 'og:description', content: 'Let your creativity take over, build without limit in your metaverse. Play with friends and the whole world' },
+      { hid: 'og-image', property: 'og:image',
+        content: 'https://artof.land/img/background-header.png'
+      },
+      { hid: 'og-url', property: 'og:url', content: 'https://artof.land' },
+      { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' },
+
+
+
     ],
     script:[
       {src:'https://unpkg.com/@metamask/legacy-web3@latest/dist/metamask.web3.min.js'},
