e68181a1658bc33fb6d71a6b5cf995ac7753e3fb
Simerca <contactsimerca@gmail.com>
2022-07-25 02:00:49 +0200

    Add some change

--- pages/map.vue
+++ pages/map.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container>
         <v-col>
-            <span class="text-h3">This is</span><br/>
+            <span class="text-h3">WATERWORLD</span><br/>
             <span class="text-h3"><span class="primary--text">LAND</span> MAP</span><br/>
             <!-- :options="{crs: L.CRS.Simple}" -->
-            <l-map :center="[0,0]" :zoom="10" v-if="this.Leaflet" :options="{crs: this.Leaflet.CRS.Simple}" style="height:500px;">
+            <l-map ref="map" :center="[0,0]" :zoom="10" v-if="this.Leaflet" :options="{crs: this.Leaflet.CRS.Simple, scale: scaleZoom}" style="height:100vh;">
             <l-tile-layer url="https://map-wtw.artof.land/api/tile/0/{x}/{y}/{z}?_=1658687347807"/>
                 <l-rectangle v-for="(chunk, i) in bounds" :bounds="chunk.bounds" :l-style="{ opacity:.4, fillOpacity:.2,fillColor: !chunk.owner ? '#C6FF08':'red',color: !chunk.owner ? '#C6FF08':'red', weight: 3 }">
                     <l-popup v-if="chunk.owner">
@@ -20,7 +20,7 @@
                         <v-card width="300px">
                             <v-card-text>
                                 <span class="text-h6">{{chunk.name}}</span><br/>
-                                <v-btn color="accent" block :href="`https://opensea.io/assets/waterworld-artofland?search[resultModel]=ASSETS&search[query]=WTW%20LAND&search[stringTraits][0][name]=ID&search[stringTraits][0][values][0]=WTW-${chunk.name}`">Buy this LAND</v-btn>
+                                <v-btn color="accent" block :href="`https://opensea.io/assets/waterworld-artofland?search[resultModel]=ASSETS&search[stringTraits][0][name]=ID&search[stringTraits][0][values][0]=WTW-${chunk.name}`">Buy this LAND</v-btn>
                             </v-card-text>
                         </v-card>
                     </l-popup>
@@ -44,18 +44,22 @@ export default {
         openseawtw:openseawtw
     }),
     methods: {
+        scaleZoom(zoom){
+        return Math.pow(2, zoom-9);
+        },
         getChunks() {
             console.log(areas);
             this.loadChunks(areas);
         },
         loadChunks(data) {
+            let diviser = 512;
             for (const chunk of data) {
                 if(chunk)
                 {
                     this.bounds.push({
                         owner: chunk.owner == "Ayrton" ? null : chunk.owner,
                         name: chunk.name,
-                        bounds: [[chunk.pos2.z / 500, chunk.pos2.x / 500], [chunk.pos1.z / 500, chunk.pos1.x/500]]
+                        bounds: [[(chunk.pos2.z + 1) / diviser, (chunk.pos2.x + 1) / diviser], [chunk.pos1.z / diviser, chunk.pos1.x/diviser]]
                     })
                 }
             }
@@ -65,6 +69,14 @@ export default {
         this.getChunks();
         this.Leaflet = L;
         console.log(this.L);
+        this.$nextTick(()=>{
+            console.log(this.$refs.map)
+            this.$refs.map.mapObject.on('mousemove', (map)=>{
+                console.log(map);
+                // console.log(map.layerPoint.x)
+                // console.log(map.layerPoint.y)
+            })
+        })
     }
 }
 </script>
